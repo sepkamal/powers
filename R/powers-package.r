c297a6bdbce5ea21707197585fa180d37bce7116
Sep <sepehr.kamal@gmail.com>
2017-11-27 22:51:23 -0800

    initial upload

---
+++ R/powers-package.r
@@ -0,0 +1,9 @@
+#' powers.
+#'
+#' @name powers
+#' @docType package
+#'
+#'
+#' WOOOO THIS IS THE package documentation for powers
+#' I made this file using: devtools::use_package_doc() in the console
+NULL
---
+++ README.md
@@ -0,0 +1,6 @@
+Readme file for Powers package
+================
+
+This is the very detailed and informative readme file for the package.
+
+SQUARE!!
